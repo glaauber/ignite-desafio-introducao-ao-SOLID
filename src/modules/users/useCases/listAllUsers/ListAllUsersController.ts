b00c1a7de00b1b6dd2fe50d80c73aecd080ae2c0
Glauber Botelho <glauber.a.botelho@gmail.com>
2021-06-27 21:18:13 -0300

    Resolucao desafio.

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,14 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+      const id: string = Array.isArray(user_id) ? user_id[0] : user_id;
+      const users = this.listAllUsersUseCase.execute({ user_id: id });
+      return response.json(users);
+    } catch (e) {
+      return response.status(400).json({ error: e.message });
+    }
   }
 }
 
